139002326fcdcadfaad075f8c29f4e5c80f9a8bb
Mayank Vats <mayank.vats@rocketfrog.ai>
2025-04-18 18:41:04 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     devIndicators: false,
+    webpack: (config) => {
+        config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+        return config;
+    },
 };
 
 export default nextConfig;
