610aa09d7cec7ce13a645b1bd778b90cb84c9216
Anubhav <anubhav.goel@rocketfrog.ai>
2025-04-22 19:58:10 +0530

    address-validation

--- src/utils/validationRules.ts
+++ src/utils/validationRules.ts
@@ -1,4 +1,6 @@
-export const getValidationRules = (type: 'text' | 'number') => ({
+export const getValidationRules = (
+  type: 'text' | 'number' | 'textAndNumber'
+) => ({
   required: 'This field is required',
   validate: (value: string) => {
     if (type === 'text' && /\d/.test(value)) {
@@ -6,6 +8,9 @@ export const getValidationRules = (type: 'text' | 'number') => ({
     }
     if (type === 'number' && !/^\d*\.?\d*$/.test(value)) {
       return 'Only numbers allowed';
+    }
+    if (type === 'textAndNumber' && !/^[a-zA-Z0-9\s]*$/.test(value)) {
+      return 'Only letters and numbers allowed';
     }
     return true;
   },
